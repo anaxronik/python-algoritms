1fef54a877986241c9535b112c08769a2818d331
Sergey Komarov <anaxronik@gmail.com>
2020-12-28 12:54:27 +0500

    1.3

---
+++ 1. Введение/1.3 Линейный алгоритм/task_2.py
@@ -0,0 +1,9 @@
+num = input('Введите трехзначное число: ')
+num = int(num)
+a = num // 100
+b = num % 100 // 10
+c = num % 10
+summa = a + b + c
+mult: int = a * b * c
+print('Сумма = ', summa)
+print('Произведение = ', mult)
